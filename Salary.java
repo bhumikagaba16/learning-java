96631c32a43ca1b78419b8b44cd7c9e54cedefb3
bhumikagaba16 <bhumigaba16@gmail.com>
2026-02-03 19:43:06 +0530

    practice java

---
+++ Salary.java
@@ -0,0 +1,12 @@
+public class Salary {
+    public static void main(String[] args) {
+        int salary =25000;
+        if(salary>=20000){
+            salary+=2000;
+        }else{
+            salary+=1000;
+        }
+        System.out.println(salary);
+    }
+    
+}
