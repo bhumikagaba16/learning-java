ad2a347babaefc21daf00f274ad4e8a1583a787c
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-02-05 20:43:19 +0530

    learning java

---
+++ Simple.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Simple {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("enter your salary");
+        int a = in.nextInt();
+        if(a>=20000){
+            a+=2000;
+        }else{
+            a+=1000;
+        }
+        System.out.println(a);
+    }
+    
+}
