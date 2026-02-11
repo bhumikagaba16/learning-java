81163160f0b6f2c47f460664bab2e9dacd7a7496
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-02-11 16:35:42 +0530

    learning java

---
+++ String4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class String4 {
+    public static void main(String[]args){
+        Scanner sc =new Scanner(System.in);
+
+        System.out.print("Customer Name: ");
+        String customerName = sc.nextLine();
+
+        System.out.print("Order Name: ");
+        String orderName =sc.nextLine();
+
+        String fullName = customerName+" ordered "+orderName+" from 5 STAR restaurant. ";
+
+        System.out.println(fullName);
+        sc.close();
+    }
+    
+}
