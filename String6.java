3e6f8b12feec843105ef6e8384d7e3356994c27a
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-02-13 21:20:30 +0530

    learning java

---
+++ String6.java
@@ -0,0 +1,9 @@
+public class String6 {
+    public static void main(String[] args) {
+        String account = "ramkripalu";
+        int age = 20;
+        String output = (account +" with age "+String.valueOf(age)+" have an account in bank of baroda.");
+        System.out.println(output);
+    }
+    
+}
