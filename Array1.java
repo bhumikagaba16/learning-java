e32a7c0ad9bf3a242727563eb8bb1cb9c0748e1a
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-02-14 21:07:28 +0530

    learning java

---
+++ Array1.java
@@ -0,0 +1,15 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int[]arr=new int[]{1,2,3,4};
+        //agar sirf arr print kre g to memory address print hogi bs
+        System.out.println(arr);
+
+        //element print krvane ke liye loop use krna hoga
+        for (int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+
+        //ya pr 
+    }
+    
+}
