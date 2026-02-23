c43b4a89212a00dda21835325a59eec6c1a09ffe
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-02-23 14:51:22 +0530

    learning java

--- SwitchCase.java
+++ SwitchCase.java
@@ -22,18 +22,37 @@ public class SwitchCase {
         //         break;
         // }
 
-        System.out.println("enter nunber for a day: ");
-        int day = in.nextInt();
-        switch (day) {
-            case 1 -> System.out.println("Monday");
-            case 2 -> System.out.println("Tuesday");
-            case 3 -> System.out.println("Wednesday");
-            case 4 -> System.out.println("Thursday");
-            case 5 -> System.out.println("Friday");
-            case 6 -> System.out.println("Saturday");
-            case 7 -> System.out.println("Sunday");
-            default -> System.out.println("invalid number of day");
+        // System.out.println("enter nunber for a day: ");
+        // int day = in.nextInt();
+        // switch (day) {
+        //     case 1 -> System.out.println("Monday");
+        //     case 2 -> System.out.println("Tuesday");
+        //     case 3 -> System.out.println("Wednesday");
+        //     case 4 -> System.out.println("Thursday");
+        //     case 5 -> System.out.println("Friday");
+        //     case 6 -> System.out.println("Saturday");
+        //     case 7 -> System.out.println("Sunday");
+        //     default -> System.out.println("invalid number of day");
                 
+        // }
+
+        int day = in.nextInt();
+
+        switch (day) {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("weekdays");    
+                break;
+            case 6:
+            case 7:
+                System.out.println("weekends");
+                break;
+            default:
+                System.out.println("invalid day");
+                break;
         }
         in.close();
     }
