7607261f08c479d88b0f08f88dd09167f4631f83
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-04-08 22:21:17 +0530

    learning java

--- Basic_program/Hello.java
+++ Basic_program/Hello.java
@@ -3,6 +3,7 @@ package Basic_program;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello beautiful!");
+        System.out.println("hello world!");
     }
     
 }
