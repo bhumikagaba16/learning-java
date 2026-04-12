0de067841d1a063c841b53e5ab8dfedbb8441b11
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-04-12 13:57:27 +0530

    learning variables in java

---
+++ Basic_program/VariableBaics.java
@@ -0,0 +1,19 @@
+package Basic_program;
+
+public class VariableBaics {
+    public static void main(String[] args) {
+        // System.out.println(89+1);
+        // System.out.println("89+1");
+
+        int x=10;
+        System.out.println(x);
+
+        int y;
+        y=20;
+        System.out.println(y);
+        y=7;
+        System.out.println(y);
+
+    }
+    
+}
