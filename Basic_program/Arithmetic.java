a5ea344f0123461e44fc7f81ba9aaf2910b4f49e
bhumikagaba16 <bhumikagaba75@gmail.com>
2026-04-22 13:47:41 +0530

    learning java

---
+++ Basic_program/Arithmetic.java
@@ -0,0 +1,16 @@
+package Basic_program;
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        // int x = 39, y = 10;
+
+        double x =39; 
+        double y = 10;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+
+        
+    }
+}
